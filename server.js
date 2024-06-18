4567a3509e3db2a8867c5578d83afd3a15b0874e
LONEWOLF143 <twaha2030@gmail.com>
2024-06-18 15:33:09 +0300

    creating node http server

--- server.js
+++ server.js
@@ -1,2 +1,12 @@
 import http from "http";
 
+const server = http.createServer((req,res) => {
+    // res.write('Hello World!!');
+    // res.setHeader('Content-Type', 'text/html')
+    // res.statusCode = 404
+
+    res.writeHead(500, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify({message: 'Server Error'}))
+})
+
+server.listen(8000, () => console.log('Server running on port 8000'))
